4ae7d4e045019f21518266eb137d20e7fd8f94b5
Adityaks <krsinghaditya98@gmail.com>
2022-09-05 03:15:36 +0530

    stack

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,38 +11,39 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count( 
-  primary: false,
-  padding: const EdgeInsets.all(20),
-  crossAxisSpacing: 10,
-  mainAxisSpacing: 10,
-  crossAxisCount: 2,
-  children: <Widget>[
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[100],
-      child: const Text("He'd have you all unravel at the"),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[200],
-      child: const Text('Heed not the r  abble'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[300],
-      child: const Text('Sound of screams but the'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[400],
-      child: const Text('Who scream'),
-    ),
-  
-    
-  ],
-)
+      body: Stack( 
+      children: [ 
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          // ignore: prefer_const_constructors
+          decoration: BoxDecoration(
+           gradient: LinearGradient( 
+            colors: [ 
+              Color(0xff213A50), Color(0xff071938)
+            ])
+          ),
+        ),
+        
+         Column( 
+          
+          children: [
+            SizedBox(height: 50,),
+            Container( 
+              
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(20),
+              child: Text("AKU Syllabus",style: TextStyle(
+                fontSize: 22,fontWeight: FontWeight.bold,color: Colors.white,
+              ),),
+            ),
+            
+          ],
+         )
+      ],
       
+      
+      )
     );
   }
 }
